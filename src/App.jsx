b6c6cc3b18512c4c2e72ab63ece576e77b16e06a
Archietheowl <adsg1206@gmail.com>
2024-10-23 15:19:46 +0100

    All done but going to replace fetch with axios now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 // src/App.jsx
 
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './pages/Landing/Landing';
+import Dashboard from './pages/Dashboard/Dashboard';
+import SignupForm from './pages/SignupForm/SignupForm';
+import SigninForm from './pages/SigninForm/SigninForm';
+import * as authService from './services/authService'
+
+
+export const AuthedUserContext = createContext(null);
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  //States
+  const [user, setUser] = useState(authService.getUser());
+  
+  //Functions/Handlers
+  const handleSignout = () => {
+    authService.signout()
+    setUser(null)
+  };
+
 
   return (
-    <>
-      <NavBar user={user} />
-      <h1>Hello world!</h1>
-    </>
+    <AuthedUserContext.Provider value={user}>
+      <NavBar handleSignout={handleSignout} />
+      <Routes>
+        {
+        user ? 
+          <Route path='/' element={<Dashboard /> } />
+        :
+          <Route path='/' element={<Landing /> }/>
+        }
+
+        <Route path='/signup' element={<SignupForm setUser={setUser} />}/>
+
+        <Route path='/signin' element={<SigninForm setUser={setUser}/>}/>
+      </Routes>
+    </AuthedUserContext.Provider>
   )
 }
 
---
+++ src/pages/SignupForm/SignupForm.jsx
@@ -0,0 +1,91 @@
+// SignupForm.jsx
+
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import * as authService from '../../services/authService'
+
+const SignupForm = (props) => {
+    //Variables
+    const navigate = useNavigate();
+    
+    //States
+    const [message, setMessage] = useState(['']);
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        passwordConf: '',
+    });
+
+    //Functions/Handlers
+    const updateMessage = (msg) => {
+        setMessage(msg);
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const newUserResponse = await authService.signup(formData)
+            props.setUser(formData) // Modify the state in the App component (so is this lifting state)
+            navigate('/')    
+        } catch (error) {
+            updateMessage(error.message)
+        }
+    };
+
+    const { username, password, passwordConf } = formData;
+
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    };
+
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={username}
+                        name="username"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        name="password"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirm"
+                        value={passwordConf}
+                        name="passwordConf"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <Link to="/">
+                        <button>Cancel</button>
+                    </Link>
+                </div>
+            </form>
+        </main>
+    );
+};
+
+export default SignupForm;
